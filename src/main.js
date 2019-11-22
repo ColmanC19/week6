1708116103c5337fb4ef01521abf74029d3c79ec
Colman Currie <curriecolman@gmail.com>
2019-11-22 12:16:44 -0800

    works on pulling info from API and outputting to html

--- src/main.js
+++ src/main.js
@@ -5,16 +5,34 @@ import './styles.css';
 
 $(document).ready(function(){
 
-  (async () => {
-      let doctor = new Doctor();
-      let response = await doctor.getDoctor(illness);
-      getElements(response);
-    })();
+  let upDateDoctor = (name, address, phoneNumber, website, newPatients) =>{
+    $("#name").text(name);
+
+    name.forEach(function(name){
+      $("#name").append(`<li>${name.data.practices} : ${name.name}</li>`);
+    })
+  }
+
 
 
 
 
   $("form.docFinder").submit(function(event){
     event.preventDefault();
+    (async () => {
+      let doctor = new Doctor();
+      let response = await doctor.getDoctor(illness);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      let keys = Object.keys(response.data);
+      for (let i=0; i < keys.length; i++){
+        let firstName = response.data[i].profile.first_name;
+        let lastName = response.data[i].profile.last_name;
+        let practiceName = response.data[i].practices[0].name;
+        let address = response.data[i].profile.title;
+      }
+    }
   });
 });
