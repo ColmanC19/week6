4e3980e12ce9b24a4b8f065f77cfc7a881ed7d73
Colman Currie <curriecolman@gmail.com>
2019-11-22 13:01:32 -0800

    add some more backend logic

--- __tests__/doctor.tests.js
+++ __tests__/doctor.tests.js
@@ -2,8 +2,8 @@ import { Doctor } from './../src/doctor.js';
 
 describe('Doctor', () =>{
   test("Should correctly allow user to search for a doctor", ()=>{
-    let doctor = new Doctor(dr stephens);
-    expect(doctor).toEqual(dr stephens);
+    let doctor = new Doctor("dr stephens");
+    expect(doctor).toEqual();
   });
 
 });
--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ import './styles.css';
 
 $(document).ready(function(){
 
-  let upDateDoctor = (name, address, phoneNumber, website, newPatients) =>{
+  let upDateDoctor = (name, address, phoneNumber, newPatients) =>{
     $("#name").text(name);
 
     name.forEach(function(name){
       $("#name").append(`<li>${name.data.practices} : ${name.name}</li>`);
+    })
+    address.forEach(function(address){
+      $("#office").append(`<li>${address.data.practices.visit_address}</li>`)
+    })
+    phoneNumber.forEach(function(phoneNumber){
+      $("#phone").append(`<li>${phoneNumber.data.practices.phones} : ${phoneNumber.number}</li>`)
+    })
+    // website.forEach(function(website){
+    //   $("#website").append(`<li>${website.data.practices.} : ${}</li>`)
+    // })
+    newPatients.forEach(function(newPatients){
+      $("#patients").append(`<li>${newPatients.data.practices.accepts_new_patients} : ${newPatients.accepts_new_patients}</li>`)
     })
   }
 
@@ -28,10 +40,10 @@ $(document).ready(function(){
     function getElements(response) {
       let keys = Object.keys(response.data);
       for (let i=0; i < keys.length; i++){
-        let firstName = response.data[i].profile.first_name;
-        let lastName = response.data[i].profile.last_name;
-        let practiceName = response.data[i].practices[0].name;
-        let address = response.data[i].profile.title;
+        let name = response.data[i].practices.name;
+        let address = response.data[i].practices.visit_address;
+        let phoneNumber = response.data[i].practices.phones.number;
+        let website = response.data[i].
       }
     }
   });
